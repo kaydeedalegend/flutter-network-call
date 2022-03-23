6643d34bef67a7a065c0267299cb96a54accd762
kaydeedalegend <mixtakaydee@gmail.com>
2022-03-24 00:46:11 +0100

    Added comments to major parts of the main.dart code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';\
+// This package helps you make network requests.
 import 'package:http/http.dart' as http;
 
+// this is the async library that enables us to write asynchronous functions.
 import 'dart:async';
+// this helps us convert json into local dart maps.
 import 'dart:convert';
 
+/// First of we create a class that represents 
+/// the data of what we will be calling from the api.
 class Album {
   final int id;
   final int userId;
@@ -15,6 +20,8 @@ class Album {
     required this.userId,
   });
 
+  /// then we create a factory function called fromJson that is 
+  /// called when ever a new instance of the class is created.
   factory Album.fromJson(Map<String, dynamic> json) {
     return Album(
       userId: json['userId'],
@@ -24,27 +31,33 @@ class Album {
   }
 }
 
+/// this is the actual function that makes the network call.
+/// the 'async' keyword indicates that it is an asynchronous function.
 Future<Album> fetchAlbum() async {
   final response = await http
       .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
 
   if (response.statusCode == 200) {
+    /// if the status code of the response we get is 200 which 
+    /// means okay, we will return a the data in a map format.
     return Album.fromJson(jsonDecode(response.body));
   } else {
+    ///else throw an exception.
     throw Exception('failed to load album');
   }
 }
 
-Future<Album> deleteAlbum(String id) async {
-  final response = await http
-      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/$id'));
+/// Ignore this for now. was trying to make a delete request.
+// Future<Album> deleteAlbum(String id) async {
+//   final response = await http
+//       .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/$id'));
 
-  if (response.statusCode == 200) {
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception("not successfully deleted");
-  }
-}
+//   if (response.statusCode == 200) {
+//     return Album.fromJson(jsonDecode(response.body));
+//   } else {
+//     throw Exception("not successfully deleted");
+//   }
+// }
 
 void main() {
   runApp(const MyApp());
@@ -58,9 +71,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  ///@override tells the app what to make it priority when starting.
   @override
+  /// Inittstate function tells the app what to do when the app loads.
   void initState() {
     super.initState();
+    /// here we tell the app to call for the api data so that we 
+    /// can get it and input into the screen asap.
     fetchAlbum();
   }
 
@@ -72,9 +89,13 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Albums'),
         ),
         body: Center(
+          /// we use a future builder here. 
+          /// it builds our widget tree based on what it gets from a future.
           child: FutureBuilder<Album>(
+            /// here we indicate what future we would like to build from.
             future: fetchAlbum(),
             builder: (context, snapshot) {
+              /// here we check if the builder has data.
               if (snapshot.hasData) {
                 return Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -84,10 +105,14 @@ class _MyAppState extends State<MyApp> {
                     Text(snapshot.data!.id.toString()),
                   ],
                 );
+                // here we check if the builder has an error.
               } else if (snapshot.hasError) {
                 return Text('$snapshot.error');
               }
 
+              /// if the builder doesn't have any data but also doesn't have any error it means that it is still leading. 
+              /// in that case we show a circular progress indicator, 
+              /// so the screen isn't blank.
               return const CircularProgressIndicator();
             },
           ),
